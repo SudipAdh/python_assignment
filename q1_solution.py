9ae7d1ee1393c46c5bc8710ca94273796e4f8ca4
Aditya-2001 <iit2019210@iiita.ac.in>
2020-07-11 22:10:25 +0530

    I have solved all the questions

--- q1_solution.py
+++ q1_solution.py
@@ -1,1 +1,11 @@
+# WAP to take the user input and devide it by certain integer. But what if user input 0 . Handle the exception.
 
+x=int(input("Enter number: "))
+y=2
+
+try:
+    y/x
+except:
+    print("Division by 0 is not possible")
+else:
+    print(y/x)
--- q3_solution.py
+++ q3_solution.py
@@ -1,1 +1,30 @@
+# WAP to make a function and pass a dictionary in the function and save the dictionary values by creating a csv file. But while creating a file do the same as above question , 1st read , if the file is found then append the dictionary and if not found , handle that error and create a new file.
 
+import csv
+def funct(dict):
+    try:
+        open("temp_file.txt","r")
+    except:
+        print("File does not exists but I will create it")
+        file=open("temp_file.txt","w")
+        file.close()
+    finally:
+        with open("temp_file.txt","a") as csv_file:
+            array=[]
+            for each in dict:
+                array.append(each)
+            writer=csv.DictWriter(csv_file,fieldnames=array)
+            writer.writeheader()
+            writer.writerow(dict)
+        file=open("temp_file.txt","r")
+        print(file.read()) 
+        
+
+
+
+name=input("Enter name: ")
+roll_no=int(input("Enter roll number: "))
+subject=input("Enter subject: ")
+
+dict = { "name": name, "roll_no": roll_no, "subject": subject}
+funct(dict)
